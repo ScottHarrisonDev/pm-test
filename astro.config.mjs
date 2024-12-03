058273242a864597d7d596ae5998ded6de0e597b
Scott Harrison <github@scottharrisondesign.co.uk>
2024-12-03 03:15:10 +0000

    update astro config for GH pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://scottharrisondev.github.io',
+  base: 'pm-test',
 });
